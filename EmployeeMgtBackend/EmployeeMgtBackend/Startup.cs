1b36ff649ddd041234abf8d399efda0d70663464
Imran2526 <imraninfo97@gmail.com>
2021-03-13 11:44:11 +0530

    [Imran] Add : Swagger Implemented in Startup File

--- EmployeeMgtBackend/EmployeeMgtBackend/Startup.cs
+++ EmployeeMgtBackend/EmployeeMgtBackend/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -34,9 +35,29 @@ namespace EmployeeMgtBackend
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddDbContext<EmployeeDBContext>
             (options => options.UseSqlServer(Configuration["Data:ConnectionStrings:DefaultConnection"]));
+            
             services.AddTransient<IEmployeeRepository, EmployeRepository>();
             services.AddTransient<IEmployeeBusines, EmployeeBusiness>();
 
+            //Swagger Implementetion
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "EmployeeManagement's", Version = "v1", Description = "EmployeeManagement" });
+                //   c.OperationFilter<FileUploadedOperation>(); ////Register File Upload Operation Filter
+                //c.DescribeAllEnumsAsStrings();
+                c.AddSecurityDefinition("Bearer", new ApiKeyScheme
+                {
+                    Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\"",
+                    Name = "Authorization",
+                    In = "header",
+                    Type = "apiKey"
+                });
+
+                c.AddSecurityRequirement(new Dictionary<string, IEnumerable<string>>
+                {
+                { "Bearer", new string[] {} }
+              });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -50,6 +71,15 @@ namespace EmployeeMgtBackend
             {
                 app.UseHsts();
             }
+
+            app.UseAuthentication();
+            app.UseAuthentication();
+            //Swagger Service 
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "EmployeeManagement");
+            });
 
             app.UseHttpsRedirection();
             app.UseMvc();
