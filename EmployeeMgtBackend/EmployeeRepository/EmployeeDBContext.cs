d96236f4f91dab98147dc6ee66e63911fc39a6c1
Imran2526 <imraninfo97@gmail.com>
2021-03-13 10:46:16 +0530

    [Imran] Add : Employee Dataset Aded

---
+++ EmployeeMgtBackend/EmployeeRepository/EmployeeDBContext.cs
@@ -0,0 +1,18 @@
+﻿using EmployeeModel;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeRepositoryLayer
+{
+    public class EmployeeDBContext : DbContext
+    {
+        public EmployeeDBContext(DbContextOptions<EmployeeDBContext> options) : base(options)
+        {
+        }
+        public DbSet<EmployeeModels> EmployeeTable { get; set; }
+        public EmployeeDBContext()
+        { }
+    }
+}
